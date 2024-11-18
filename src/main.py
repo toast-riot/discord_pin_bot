be631b7ca0f341c79e0eb7f4791f336d17c5c2ed
nex_e6 <nex_e6@outlook.com>
2024-11-18 15:02:44 +1000

    refactor: cleanup, dual embeds style and webhook helper function

--- src/main.py
+++ src/main.py
@@ -9,51 +9,49 @@ def main():
     async def channel_by_name(guild: discord.Guild, name: str):
         return discord.utils.get(guild.text_channels, name=name)
 
-    # async def message_pin_or_unpin(interaction: discord.Interaction, message: discord.Message):
-    #     if message.channel.permissions_for(message.guild.me).manage_messages == False:
-    #         await response_followup(interaction, "This bot does not have permission to pin messages here")
-    #         return
-    #     pinned = message.pinned
-    #     if pinned:
-    #         await message.unpin()
-    #     else:
-    #         try:
-    #             await message.pin()
-    #         except discord.errors.HTTPException as e:
-    #             if e.code == 30003:
-    #                 await response_followup(interaction, "Pin limit reached")
-    #                 return
-    #     await response_followup(interaction, f"Message {'un' if pinned else ''}pinned")
-
 
     async def get_embed(message: discord.Message):
-        embed = discord.Embed(
+        main_embed = discord.Embed(
             description = message.content,
             color = 0x2b2d31,
             # timestamp = message.created_at
         )
-        embed.add_field(name="", value="", inline=False) # Spacer
+        # main_embed.add_field(name="", value="", inline=False) # Spacer
 
         if message.embeds:
             data = message.embeds[0]
             if data.type == "image":
-                embed.set_image(url=data.url)
+                main_embed.set_image(url=data.url)
 
         if message.attachments:
             attachment = message.attachments[0]
             path = urlparse(attachment.url).path
             if path.lower().endswith(("png", "jpeg", "jpg", "gif", "webp")):
-                embed.set_image(url=attachment.url)
+                main_embed.set_image(url=attachment.url)
             else:
-                embed.add_field(name="Attachment", value=f"-# {attachment.url}", inline=False)
+                main_embed.add_field(name="Attachment", value=f"-# {attachment.url}", inline=False)
 
-        embed.add_field(name="User", value=f"-# {message.author.mention}")
-        embed.add_field(name="Link", value=f"-# {message.jump_url}")
+        secondary_embed = discord.Embed(
+            color = 0x2b2d31
+        )
+        secondary_embed.add_field(name="User", value=f"-# {message.author.mention}")
+        secondary_embed.add_field(name="Link", value=f"-# {message.jump_url}")
 
-        embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+        main_embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
         # embed.set_footer(text=f"#{message.channel.name}")
 
-        return embed
+        return [main_embed, secondary_embed]
+
+
+    async def new_webhook(channel: discord.TextChannel, username: str, avatar_url: str, content):
+        webhooks = await channel.webhooks()
+        webhook = discord.utils.get(webhooks, name='temp')
+
+        if not webhook:
+            webhook = await channel.create_webhook(name='temp', avatar=None)
+
+        await webhook.send(content=content, username=username, avatar_url=avatar_url)
+        await webhook.delete()
 
 
     async def response_followup(interaction: discord.Interaction, content: str):
@@ -72,13 +70,13 @@ def main():
 
 
     async def pinboard(interaction: discord.Interaction, message: discord.Message):
-        embed = await get_embed(message)
+        embeds = await get_embed(message)
 
         if message.channel.is_nsfw():
             pin_channel = await channel_by_name(message.guild, CONFIG["nsfw_pins_channel"])
         else:
             pin_channel = await channel_by_name(message.guild, CONFIG["pins_channel"])
-        pin_message = await pin_channel.send(embed=embed, allowed_mentions=discord.AllowedMentions.none())
+        pin_message = await pin_channel.send(embeds=embeds, allowed_mentions=discord.AllowedMentions.none())
         await response_followup(interaction, f"Message pinned to <#{pin_message.channel.id}>")
 
 
@@ -106,12 +104,10 @@ def main():
 
 
     @bot.event
-    async def on_message(message: discord.Message): # This stops the bot checking for commands
+    async def on_message(message: discord.Message): # This intentionally prevents the bot checking for plaintext commands
         pass
         # await bot.tree.sync()
         # print(f"{message.author}: {message.content}")
-        # if message.author == bot.user and message.type == discord.MessageType.pins_add:
-        #     await message.delete()
 
 
     @bot.event
@@ -145,7 +141,9 @@ def main():
         name="test"
     )
     async def test(interaction: discord.Interaction, user: discord.User=None, channel: discord.TextChannel=None):
-        await interaction.response.send_message(f"{user.name} {user.global_name} {user.display_name} {user.mention} {user.id}")
+        # await interaction.response.send_message(f"{user.name} {user.global_name} {user.display_name} {user.mention} {user.id}")
+        await new_webhook(channel, user.name, user.display_avatar.url, "Test")
+        await interaction.response.send_message("Done")
 
 
     @bot.tree.context_menu(
@@ -154,30 +152,6 @@ def main():
     async def channel_pin_message_context(interaction: discord.Interaction, message: discord.Message):
         await interaction.response.defer()
         await pinboard(interaction, message)
-
-
-    # @bot.tree.command(
-    #     name="count_pins",
-    #     description="Get the number of pinned messages in a channel",
-    # )
-    # async def count_pins(interaction: discord.Interaction, channel: discord.TextChannel = None):
-    #     await interaction.response.defer()
-    #     if channel is None:
-    #         channel = interaction.channel
-    #     try:
-    #         pins = await channel.pins()
-    #     except discord.errors.Forbidden:
-    #         await response_followup(interaction, f"This bot does not have permission to access <#{channel.id}>")
-    #         return
-    #     await response_followup(interaction, f"<#{channel.id}> has {len(pins)}/50 pinned messages")
-
-
-    # @bot.tree.context_menu(
-    #     name="Pin / Unpin Message"
-    # )
-    # async def pin_message_context(interaction: discord.Interaction, message: discord.Message):
-    #     await interaction.response.defer()
-    #     await message_pin_or_unpin(interaction, message)
 
 
     #- Start
